cacd7d05d65900aecdb3163dc68e020138b5a7f1
XiLiuRhys <xliu0258@student.monash.edu>
2025-05-01 14:05:57 +1000

    Update urls.py
    add api/get-uv-index/ urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('api/postcode-scores/', views.get_postcode_scores, name='postcode_scores'),
     path('api/accidents/', views.get_nearby_accidents, name='accident-data'),
     path('api/analyze-accidents/', views.analyze_accidents, name='analyze_accidents_data'),
-    path('api/analyze-location/', views.get_postcode_by_coordinate, name='get_postcode_by_coordinate')
+    path('api/analyze-location/', views.get_postcode_by_coordinate, name='get_postcode_by_coordinate'),
+    path('api/get-uv-index/', views.get_uv_index_by_year)
 ]
