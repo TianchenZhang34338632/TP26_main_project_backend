acccc200d471cac9f1f8fda945c1c2c326177a63
Jack <tzha0152@student.monash.edu>
2025-05-22 17:05:05 +1000

    Change DEBUG mode

--- TP26_main_project_backend/settings.py
+++ TP26_main_project_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7cf8yv*4-=4*=&(13bcl0xk=i%zx-7hrqzew)21ne%pep+jz84'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
